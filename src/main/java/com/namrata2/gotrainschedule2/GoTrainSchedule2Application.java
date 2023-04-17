1622b413b654c7a02e9bb5cc6bf34a2e93f7ea7f
danguinamrata <danguinamrata@gmail.com>
2023-04-17 14:57:15 -0400

    Initial barebones setup

---
+++ src/main/java/com/namrata2/gotrainschedule2/GoTrainSchedule2Application.java
@@ -0,0 +1,13 @@
+package com.namrata2.gotrainschedule2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoTrainSchedule2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoTrainSchedule2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/namrata2/gotrainschedule2/GoTrainSchedule2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.namrata2.gotrainschedule2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoTrainSchedule2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
