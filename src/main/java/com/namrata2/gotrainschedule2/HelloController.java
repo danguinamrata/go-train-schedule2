662ca25417c6063b67857ab5ece381976e980a8d
danguinamrata <danguinamrata@gmail.com>
2023-04-17 15:07:31 -0400

    Added basic hello world endpoint

---
+++ src/main/java/com/namrata2/gotrainschedule2/HelloController.java
@@ -0,0 +1,12 @@
+package com.namrata2.gotrainschedule2;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/")
+    public String index() {
+        return "Greetings from Go Train!";
+    }
+}
